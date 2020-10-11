fd3006f3a77d252c1cc139811a4adfd79c6f644f
Alexandr Zeinalov <azeinalov@mfms.ru>
2020-10-11 20:39:02 +0300

    inline mode and test mode

--- bot.py
+++ bot.py
@@ -30,15 +30,10 @@ def translate(code, text):
 
 load_dict("glag2cyrl","glag2cyrl.tab")
 load_dict("tfng2cyrl","tfng2cyrl.tab")
+load_dict("cyrl2glag","cyrl2glag.tab")
+load_dict("cyrl2tfng","cyrl2tfng.tab")
 
 bot = telebot.TeleBot(config.bot_token)
-
-@bot.message_handler(commands=["rules"])
-def rules(message):
-  if message.chat.type in ['group','supergroup'] and message.chat.id == -1001199017575:
-    with open("rules.md", "r") as f:
-      rules = f.read()
-    bot.send_message(message.chat.id, rules, parse_mode="Markdown")
 
 @bot.message_handler(content_types=['text'])
 def translate_message(message):
@@ -47,17 +42,40 @@ def translate_message(message):
   if time() > message.date+config.max_timediff:
     print (" message time too old :(")
     return
-  for code in dictmap:
+  for code in config.default_tabs:
     msgtr = translate(code, msg)
     dist = levenshtein_distance(msg, msgtr)
     ratio = dist/len(msg)
     if ratio > config.min_levenshtein_ratio:
       print (" code=%s ratio=%lf => %s" % (code, ratio, msgtr))
       try:
+        if config.test_mode:
+          msgtr = "[TEST MODE] "+msgtr
         bot.send_message(message.chat.id, msgtr, reply_to_message_id=message.message_id)
       except telebot.apihelper.ApiException:
         print (" Exception occured!")
       return
+
+# команда /rules для @mikitkinabeseda
+@bot.message_handler(commands=["rules"])
+def rules(message):
+  if message.chat.type in ['group','supergroup'] and message.chat.id == -1001199017575:
+    with open("rules.md", "r") as f:
+      rules = f.read()
+    bot.send_message(message.chat.id, rules, parse_mode="Markdown")
+
+# inline-режим, у боевого бота выключен, так как нельзя запретить использовать его в конкретном чате
+@bot.inline_handler(lambda query: len(query.query) > 0)
+def query_text(inline_query):
+  print (inline_query)
+  msgtr_glag = translate('cyrl2glag', inline_query.query)
+  msgtr_tfng = translate('cyrl2tfng', inline_query.query)
+  if config.test_mode:
+    msgtr_glag = "[TEST MODE] "+msgtr_glag
+    msgtr_tfng = "[TEST MODE] "+msgtr_tfng
+  r_glag = telebot.types.InlineQueryResultArticle('GLAG', f'{inline_query.query} -> {msgtr_glag}', telebot.types.InputTextMessageContent(msgtr_glag))
+  r_tfng = telebot.types.InlineQueryResultArticle('TFNG', f'{inline_query.query} -> {msgtr_tfng}', telebot.types.InputTextMessageContent(msgtr_tfng))
+  bot.answer_inline_query(inline_query.id, [r_glag, r_tfng])
 
 while True:
   try:
